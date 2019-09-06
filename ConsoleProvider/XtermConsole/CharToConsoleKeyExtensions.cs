76b4c06712c0cf06b94f0ff7b61076dd2b9ceb57
Xinran Huang <DreamRy\Mizunara>
2019-09-06 14:40:21 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/FoggyConsole;C278

---
+++ ConsoleProvider/XtermConsole/CharToConsoleKeyExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Globalization;
+
+namespace DreamRecorder.FoggyConsole.XtermConsole
+{
+    public static class CharToConsoleKeyExtensions
+    {
+        public static ConsoleKeyInfo? ToConsoleKey(this char c)
+        {
+            if (Enum.TryParse(
+                                  c.ToString(CultureInfo.InvariantCulture),
+                                  true,
+                                  out ConsoleKey consoleKey))
+            {
+                return new ConsoleKeyInfo(c, consoleKey, char.IsUpper(c), false, false);
+            }
+            else
+            {
+                switch (c)
+                {
+                    case ' ':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Spacebar, false, false, false);
+                    case '\u0009':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Tab, false, false, false);
+                    case ';':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem1, false, false, false);
+                    case ':':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem1, true, false, false);
+                    case '/':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem2, false, false, false);
+                    case '?':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem2, true, false, false);
+                    case '`':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem3, false, false, false);
+                    case '~':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem3, true, false, false);
+                    case '[':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem4, false, false, false);
+                    case '{':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem4, true, false, false);
+                    case '\\':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem5, false, false, false);
+                    case '|':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem5, true, false, false);
+                    case ']':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem6, false, false, false);
+                    case '}':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Oem6, true, false, false);
+                    case ',':
+                        return new ConsoleKeyInfo(c, ConsoleKey.OemComma, false, false, false);
+                    case '<':
+                        return new ConsoleKeyInfo(c, ConsoleKey.OemComma, true, false, false);
+                    case '.':
+                        return new ConsoleKeyInfo(c, ConsoleKey.OemComma, false, false, false);
+                    case '>':
+                        return new ConsoleKeyInfo(c, ConsoleKey.OemComma, true, false, false);
+                    case '-':
+                        return new ConsoleKeyInfo(c, ConsoleKey.OemMinus, false, false, false);
+                    case '_':
+                        return new ConsoleKeyInfo(c, ConsoleKey.OemMinus, true, false, false);
+                    case '=':
+                        return new ConsoleKeyInfo(c, ConsoleKey.OemPlus, false, false, false);
+                    case '+':
+                        return new ConsoleKeyInfo(c, ConsoleKey.OemPlus, true, false, false);
+                    case '\r':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Enter, false, false, false);
+                    case '\u001b':
+                        return new ConsoleKeyInfo(c, ConsoleKey.Escape, false, false, false);
+                    default:
+                        return null;
+                }
+
+            }
+
+        }
+
+    }
+
+}
--- ConsoleProvider/XtermConsole/EscapeSequenceParsers/KeyEscapeSequenceParser.cs
+++ ConsoleProvider/XtermConsole/EscapeSequenceParsers/KeyEscapeSequenceParser.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace DreamRecorder.FoggyConsole.XtermConsole
 {
-
     public class KeyEscapeSequenceParser : IInputEscapeSequenceParser
     {
 
@@ -69,63 +66,21 @@ namespace DreamRecorder.FoggyConsole.XtermConsole
                             console.InvokeKeyPressed(new ConsoleKeyInfo(default, ConsoleKey.LeftArrow, false, false, false));
                             break;
                         }
+                    case "H":
+                        {
+                            console.InvokeKeyPressed(new ConsoleKeyInfo(default, ConsoleKey.Home, false, false, false));
+                            break;
+                        }
+                    case "F":
+                        {
+                            console.InvokeKeyPressed(new ConsoleKeyInfo(default, ConsoleKey.End, false, false, false));
+                            break;
+                        }
                     default:
                         {
                             break;
                         }
                 }
-
-                content.RemoveRange(0, match.Value.Length);
-            }
-        }
-
-    }
-
-
-    public class ConsoleSizeEscapeSequenceParser : IInputEscapeSequenceParser
-    {
-
-        public Regex FullEscape = new Regex(@"^\u001b\[8;(\d+);(\d+)t");
-
-        public Regex TryEscape = new Regex(@"^\u001b(?:\[|$)(?:8|$)(?:;|$)(?:(\d+)|$)(?:;|$)(?:(\d+)|$)(?:t|$)");
-
-        public ParseResult TryParse(List<char> content)
-        {
-            string contentString = new string(content.ToArray());
-
-            Match tryMatch = TryEscape.Match(contentString);
-
-            if (tryMatch.Success)
-            {
-                Match fullMatch = FullEscape.Match(contentString);
-
-                if (fullMatch.Success)
-                {
-                    return ParseResult.Finished;
-                }
-                else
-                {
-                    return ParseResult.Established;
-                }
-            }
-            else
-            {
-                return ParseResult.CanNotEstablish;
-            }
-        }
-
-        public void Apply(List<char> content, XtermConsole console)
-        {
-            string contentString = new string(content.ToArray());
-
-            Match match = FullEscape.Match(contentString);
-
-            if (match.Success)
-            {
-                int height = Convert.ToInt32(match.Groups[1].Value);
-                int width = Convert.ToInt32(match.Groups[2].Value);
-
-                console.InternalSize = new Size(width, height);
 
                 content.RemoveRange(0, match.Value.Length);
             }
