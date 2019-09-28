414c8ff5236d05a9dc287273a00337f52ad76e55
Wencey Wang <WenceyWang@dreamry.org>
2019-09-28 19:11:50 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/FoggyConsole;C341

--- FoggyConsole/LineStyle.cs
+++ FoggyConsole/LineStyle.cs
@@ -77,6 +77,11 @@ namespace DreamRecorder . FoggyConsole
 
 			public override object ConvertFrom ( ITypeDescriptorContext context , CultureInfo culture , object value )
 			{
+				if ( value is null )
+				{
+                    return null;
+				}
+
 				if ( value is string lineStyleData )
 				{
 					return ( LineStyle ) lineStyleData ;
